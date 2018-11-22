4aa1ad7a3c8c146299a317406a7dd037fa16f8af
fuwenqing <18679956837@163.com>
2018-11-23 00:00:56 +0800

    springcloud

--- microservice-consumer-movie/src/main/java/com/itmuch/cloud/contraller/MovieContraller.java
+++ microservice-consumer-movie/src/main/java/com/itmuch/cloud/contraller/MovieContraller.java
@@ -3,6 +3,7 @@ package com.itmuch.cloud.contraller;
 import com.itmuch.cloud.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,9 +15,9 @@ public class MovieContraller {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/simple/user")
-    public User gerUser(){
-        User user = restTemplate.getForObject("https://idasc.test.webank.com/api/oauth2/access_token?app_id=xxx&secret=xxx&grant_type=client_credential&version=1.0.0", User.class);
+    @GetMapping("/simple/{id}")
+    public User gerUser(@PathVariable Long id){
+        User user = restTemplate.getForObject("http://microservice-provider-user/simple/"+id, User.class);
         return  user;
     }
 
