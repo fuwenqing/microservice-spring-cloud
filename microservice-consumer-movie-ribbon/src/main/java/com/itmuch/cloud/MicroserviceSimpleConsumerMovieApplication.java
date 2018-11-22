01fcda1ac7e3f90b6221c1b5125fd707a458dfb3
fuwenqing <18679956837@163.com>
2018-11-22 23:07:48 +0800

    343

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -0,0 +1,34 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+@EnableEurekaClient
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class MicroserviceSimpleConsumerMovieApplication {
+
+	@Bean
+	public RestTemplate restTemplate(){
+		RestTemplate restTemplate=new RestTemplate();
+		return  restTemplate;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+	}
+}
