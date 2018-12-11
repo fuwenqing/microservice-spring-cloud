00c67079395862f6b48747a1aacff37be58fd464
fuwenqing <18679956837@163.com>
2018-12-11 22:28:20 +0800

    springcloud

---
+++ microservice-consumer-movie-hystrix/src/main/java/com/itmuch/cloud/contraller/MovieContraller.java
@@ -0,0 +1,30 @@
+package com.itmuch.cloud.contraller;
+
+import com.itmuch.cloud.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class MovieContraller {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/simple/{id}")
+    public User gerUser(@PathVariable Long id) {
+        User user = restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+        return user;
+    }
+
+    public void fallback() {
+        System.out.println("4343");
+    }
+
+
+}
---
+++ microservice-consumer-movie-hystrix/src/main/java/com/itmuch/cloud/entity/User.java
@@ -0,0 +1,69 @@
+package com.itmuch.cloud.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+public class User {
+    private  String code;
+    private  String msg;
+    private long id;
+    private String username;
+    private String name;
+    private short age;
+    private BigDecimal balance;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
